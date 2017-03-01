cd979c0aab3aeb71a15ca7ad2f624bb8f11a1cbf
xiaowu <570518277@qq.com>
2017-03-01 21:39:46 +0800

    '良品精选内容'

--- component/goodproducts/goodproducts.js
+++ component/goodproducts/goodproducts.js
@@ -12,9 +12,14 @@ angular.module("goodproductsModule",['ui.router'])
 
 .service('goodproductsData',['$http',function($http){
 	this.get=function(){
-		return $http.get('json/5.json');
+		return $http.get('component/goodproducts/json/nondefective.json');
+	}
+	this.getpart1=function(){
+		return $http.get('component/goodproducts/json/nondefective_1.json');
 	}
 }])
+
+
 .service('swipe',['$timeout',function($timeout){
 	this.swipe=function(){
 		$timeout(function(){
@@ -28,10 +33,28 @@ angular.module("goodproductsModule",['ui.router'])
 		},50);
 	}
 }])
+
+
 .controller('goodproductsCtrl',['$scope','goodproductsData','swipe',function($scope,goodproductsData,swipe){
 	goodproductsData.get().success(function(res){
-		$scope.arr = res.product;
-		console.log(res.product)
+		$scope.obj = res.data;
+		//轮播图数据
+		$scope.swipeData=res.data[7290].list;
 		swipe.swipe();
+
+		//限时抢购数据
+		$scope.limitedData=res.data[8927].list;
+		//蘑菇优选数据
+		$scope.good_introduceData=res.data[7286].list;
+
+		//良品推荐数据
+		$scope.lp_goodsData=res.data[11303].list;
+
 	})
+	
+	goodproductsData.getpart1().success(function(res){
+		$scope.data=res.result;
+		console.log($scope.data)
+	})
+	
 }])
