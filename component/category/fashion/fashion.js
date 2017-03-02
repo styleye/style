b8c2ca8dd1a88490532b5ed7240b0f453f00f581
zangrui <260037396@qq.com>
2017-03-02 21:48:35 +0800

    '分类下二级页面静态版----ZR'

--- component/category/fashion/fashion.js
+++ component/category/fashion/fashion.js
@@ -3,6 +3,37 @@ angular.module('FashionModule',['ui.router'])
 	$stateProvider
 	.state('category.fashion',{
 		url:'/fashion',
-		templateUrl:'component/category/fashion/fashion.html'
+		templateUrl:'component/category/fashion/fashion.html',
+		controller:'FashionCtrl',
+		css:'component/category/fashion/fashion.css'
 	})
 })
+
+.service('fashionData',['$http',function($http){
+	this.get = function(){
+		return $http.get('component/category/json/fashion.json')
+	}
+}])
+
+//.directive("scroll",function($window){
+//	return function(scope,element,attrs){
+//		angular.element($window).bind("scroll",function(){
+//			console.log(this.pageYOffset);
+//			scope.$apply();
+//		})
+//	}
+//})
+.controller('FashionCtrl',['$scope','$location','$anchorScroll','fashionData',function($scope,$location,$anchorScroll,fashionData){
+	fashionData.get().success(function(res){
+		console.log(res.result)
+		$scope.arr = res.result
+	})
+	$scope.flag = false
+	$scope.toggle = function(){
+		$scope.flag = !$scope.flag;
+	}
+	$scope.goto = function(id){
+		$location.hash(id);
+		$anchorScroll();
+	}
+}])
